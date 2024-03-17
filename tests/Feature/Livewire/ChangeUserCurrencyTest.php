b892741dfc41829f131d75aa6d12de46b3d22b44
Anton <loginovanton95@gmail.com>
2024-03-17 23:07:35 +0100

    first release of the app

--- tests/Feature/Livewire/ChangeUserCurrencyTest.php
+++ tests/Feature/Livewire/ChangeUserCurrencyTest.php
@@ -3,9 +3,12 @@
 declare(strict_types=1);
 
 use App\Livewire\ChangeUserCurrency;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
-    Livewire::test(ChangeUserCurrency::class)
+    $user = User::factory()->create();
+    Livewire::actingAs($user)
+        ->test(ChangeUserCurrency::class)
         ->assertStatus(200);
 });
