8c00810f51703d2d5bd8c86e8b99384c63f5d038
Anton <loginovanton95@gmail.com>
2024-03-18 14:04:06 +0100

    fix - added resend mail driver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,9 @@ use App\Crypto\Contracts\ICryptoClient;
 use App\Crypto\Contracts\IExchangeClient;
 use App\MarketData\Clients\AlphaVantageClient;
 use App\MarketData\Contracts\IStockMarketClient;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -41,6 +44,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(static function ($notifiable, $url) {
+            return (new MailMessage)
+                ->mailer('resend')
+                ->subject(Lang::get('Verify Email Address'))
+                ->line(Lang::get('Please click the button below to verify your email address.'))
+                ->action(Lang::get('Verify Email Address'), $url)
+                ->line(Lang::get('If you did not create an account, no further action is required.'));
+        });
     }
 }
