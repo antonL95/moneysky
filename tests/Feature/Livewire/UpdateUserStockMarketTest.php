00d815bcfa72c0eddd8cd794dc9c120abc5c1c72
Anton <loginovanton95@gmail.com>
2024-03-18 21:30:03 +0100

    fix - update forms

--- tests/Feature/Livewire/UpdateUserStockMarketTest.php
+++ tests/Feature/Livewire/UpdateUserStockMarketTest.php
@@ -3,9 +3,23 @@
 declare(strict_types=1);
 
 use App\Livewire\UpdateUserStockMarket;
+use App\MarketData\Models\UserStockMarket;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
-    Livewire::test(UpdateUserStockMarket::class)
-        ->assertStatus(200);
+    $user = User::factory()->create();
+
+    $ticker = UserStockMarket::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    Livewire::actingAs($user)
+        ->test(UpdateUserStockMarket::class, [
+            'ticker' => $ticker,
+        ])
+        ->set('form.amount', 69.69)
+        ->call('update', $ticker);
+
+    expect($ticker->refresh()->amount)->toBe(69.69);
 });
