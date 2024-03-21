9e6046519ff208019994867e923509995c04d577
Anton <loginovanton95@gmail.com>
2024-03-21 16:09:38 +0100

    fix - styling issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ export default {
     ],
     safelist: [
         {pattern: /(bg|text|border|grid|table)-./},
-        {pattern: /(grid|flex|hidden)./},
         {pattern: /(grid|flex|hidden)./}
     ],
 
