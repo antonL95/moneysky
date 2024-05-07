87a257dcbc6320591313ffbae55a5fcd61cc31d6
Anton <loginovanton95@gmail.com>
2024-05-07 16:54:46 +0200

    update dependencies and deploy script simplification

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,26 +2,52 @@
     <x-authentication-card>
         <x-slot:headline>
             {{ __('Reset password') }}
-        </x-slot:headline>
+        </x-slot>
 
         <form method="POST" action="{{ route('password.update') }}">
             @csrf
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+            <input type="hidden" name="token" value="{{ $request->route('token') }}" />
 
             <div class="block">
-                <x-ts-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus label="{{ __('Email') }}" />
-
-            <div class="mt-4">
-                <x-ts-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" label="{{ __('Password') }}" />
+                <x-ts-input
+                    id="email"
+                    class="mt-1 block w-full"
+                    type="email"
+                    name="email"
+                    :value="old('email', $request->email)"
+                    required
+                    autofocus
+                    label="{{ __('Email') }}"
+                />
             </div>
 
             <div class="mt-4">
-                <x-ts-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" label="{{ __('Confirm Password') }}" />
+                <x-ts-input
+                    id="password"
+                    class="mt-1 block w-full"
+                    type="password"
+                    name="password"
+                    required
+                    autocomplete="new-password"
+                    label="{{ __('Password') }}"
+                />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button type="submit" :title="__('Reset Password')"/>
+            <div class="mt-4">
+                <x-ts-input
+                    id="password_confirmation"
+                    class="mt-1 block w-full"
+                    type="password"
+                    name="password_confirmation"
+                    required
+                    autocomplete="new-password"
+                    label="{{ __('Confirm Password') }}"
+                />
+            </div>
+
+            <div class="mt-4 flex items-center justify-end">
+                <x-button type="submit" :title="__('Reset Password')" />
             </div>
         </form>
     </x-authentication-card>
