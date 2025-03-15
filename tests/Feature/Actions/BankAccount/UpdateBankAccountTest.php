16428cc59f8580c38978716fdab95faf437e91f7
Anton <loginovanton95@gmail.com>
2025-03-15 13:18:33 +0100

    added homepage and additional updates

--- tests/Feature/Actions/BankAccount/UpdateBankAccountTest.php
+++ tests/Feature/Actions/BankAccount/UpdateBankAccountTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Actions\BankAccount\UpdateBankAccount;
 use App\Data\App\BankAccount\BankAccountData;
 use App\Models\UserBankAccount;
+
 use function Pest\Laravel\assertDatabaseHas;
 
 it('successfully updates', function () {
