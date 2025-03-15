d8cdfa25ed467773e01100a4f10be949f3871f80
Anton <loginovanton95@gmail.com>
2025-03-16 00:55:28 +0100

    added homepage and additional updates

--- database/factories/TransactionTagFactory.php
+++ database/factories/TransactionTagFactory.php
@@ -14,7 +14,18 @@ final class TransactionTagFactory extends Factory
     public function definition(): array
     {
         return [
-            'tag' => $this->faker->word(),
+            'tag' => $this->faker->randomElement([
+                'Groceries',
+                'Dining Out',
+                'Utilities',
+                'Rent/Mortgage',
+                'Streaming Services',
+                'Online Subscriptions',
+                'Health & Wellness',
+                'Shopping',
+                'Transportation',
+                'Savings & Investments',
+            ]),
             'color' => $this->faker->hexColor(),
         ];
     }
